75544f20e5eb2c16283cfe050bb2484fc4b1b3a7
Steve Gustus <43791455+sngustus@users.noreply.github.com>
2025-12-11 19:56:42 -0500

    add drag and drop for player caravan

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { Deck, Hand, Caravan, Card } from './objects.js';
 //setup
 let playerDeck = new Deck();
 let playerHand = new Hand(playerDeck);
+let draggedCard = null;
 
 const playerHandDiv = document.getElementById('player-hand');
 //deal hand
@@ -12,6 +13,7 @@ playerHand.cards.forEach((card, index) => {
     const cardImg = document.createElement('img');
     cardImg.src = card.imgSrc;
     cardImg.alt = `Card ${index + 1}`;
+    cardImg.classList.add('card');
     playerHandDiv.appendChild(cardImg);
 });
 
@@ -27,4 +29,27 @@ document.addEventListener('DOMContentLoaded', () => {
         gameDiv.style.display = 'block';
     });
 
+    //event listeners for player hand cards
+    const handCards = playerHandDiv.querySelectorAll('.card');
+    handCards.forEach((cardImg, index) => {
+        cardImg.addEventListener('dragstart', (e) => {
+            draggedCard = playerHand.cards[index];
+        });
+    });
+
+    //event listeners for caravans
+    const caravans = document.querySelectorAll('.caravan');
+    caravans.forEach(caravan => {
+        caravan.addEventListener('drop', () => {
+            if (draggedCard) {
+                caravan.cards.push(draggedCard); //add card to caravan
+                playerHand.cards = playerHand.cards.filter(c => c !== draggedCard); //remove card from hand
+                console.log('Card played:', draggedCard); //for testing
+                draggedCard = null;
+
+                caravan.evaluateCaravan(0); //evaluate caravan after card is played
+            }});
+    });
+
 });
+
